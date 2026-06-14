b45e04097de974e497faeb8eb418b3a0f1c9c353
mehedihasanop <mh3589605@gmail.com>
2026-06-14 19:57:09 +0600

    Update supabaseServer.js

--- supabaseServer.js
+++ supabaseServer.js
@@ -3,7 +3,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = pr ocess.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export function getSupabaseServerClient() {
   return createClient(supabaseUrl, supabaseAnonKey, {
