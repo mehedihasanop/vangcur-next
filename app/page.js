50f5a388549b07dec733b3a260ea8b1bb5261c6a
mehedihasanop <mh3589605@gmail.com>
2026-07-22 16:21:57 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
-import Navbar from './components/layout/Navbar';
+'use client';
+import dynamic from 'next/dynamic';
+
+const Navbar = dynamic(() => import('./components/layout/Navbar'), { ssr: false });
 
 export default function HomePage() {
   return (
