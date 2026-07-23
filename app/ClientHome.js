5c3e40dbeccb0dd10ddde468d048ebf3e2de1e96
mehedihasanop <mh3589605@gmail.com>
2026-07-23 07:04:32 +0600

    Add HeroSlider component to ClientHome

--- app/ClientHome.js
+++ app/ClientHome.js
@@ -1,11 +1,13 @@
 'use client';
 import Navbar from './components/layout/Navbar';
+import HeroSlider from './components/home/HeroSlider';
 
 export default function ClientHome() {
   return (
     <>
       <div className="toast" id="toast"></div>
       <Navbar />
+      <HeroSlider />
     </>
   );
 }
