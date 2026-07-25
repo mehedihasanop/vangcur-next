9cbdfbded039f6e45f64805a703ce6725eb3bbf1
mehedihasanop <mh3589605@gmail.com>
2026-07-25 15:47:54 +0600

    Update Navbar.js

--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -112,7 +112,7 @@ export default function Navbar({ cartCount = 0, wishCount = 0, onCartClick, onWi
                 <svg width="20" height="20" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                   <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"/>
                 </svg>
-                <span className="cart-dot" id="wishDot">{wishCount}</span>
+                <span className={`cart-dot${wishCount > 0 ? ' on' : ''}`} id="wishDot">{wishCount}</span>
               </button>
 
               <button className="nav-icon-btn" onClick={onCartClick}>
@@ -191,4 +191,4 @@ export default function Navbar({ cartCount = 0, wishCount = 0, onCartClick, onWi
       </div>
     </>
   );
-      }
+}
